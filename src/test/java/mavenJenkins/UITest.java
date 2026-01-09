66387bee52bca20f061ceed91eb24fbd457b5698
kharatb24 <bhakti.kharate@virginmediao2.co.uk>
2026-01-09 14:49:51 +0530

    Update UITest.java

--- src/test/java/mavenJenkins/UITest.java
+++ src/test/java/mavenJenkins/UITest.java
@@ -3,10 +3,25 @@ package mavenJenkins;
 import org.testng.annotations.Test;
 
 public class UITest {
-	
+
+	WebDriver driver;
 	@Test
 	void firstFunc() {
 		System.out.println("hello!!!!!!");
+	}
+
+	
+
+	@Test
+	public void verifyTitle() {
+	//WebDriverManager.chromedriver().setup();
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("headless");
+	driver=new ChromeDriver(options);
+	driver.get("https://www.browserstack.com/");
+	System.out.println("Title is: " +driver.getTitle());
+	Assert.assertEquals(driver.getTitle(), "Most Reliable App & Cross Browser Testing Platform | BrowserStack");
+	driver.quit();
 	}
 
 }
